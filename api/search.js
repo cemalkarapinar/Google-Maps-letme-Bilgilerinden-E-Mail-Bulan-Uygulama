e280a019fd6175a8cd34f261eca9ea1c6bcd3b21
Cemal Karapınar <cemalkarapinar1997@gmail.com>
2025-09-01 17:00:28 +0300

    Node.js versiyonu 22.x'e güncellendi - Vercel gereksinimi

--- api/search.js
+++ api/search.js
@@ -1,5 +1,5 @@
 // Vercel serverless function - Gerçek veri çekme
-// Node.js 18.x runtime
+// Node.js 22.x runtime
 export default async function handler(req, res) {
     // CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
